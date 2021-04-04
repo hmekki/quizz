3c2581fba8d32e3c339068494344a61f0cc8d3dd
hassen mekki <hassen.mekki@gmail.com>
2021-04-05 01:20:33 +0200

    change port

--- server.js
+++ server.js
@@ -20,11 +20,18 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-app.listen(5000,() => {
-   console.log('Express server started at port 3000'); 
-})
 
-app.use('/',userController);
+
+const PORT = process.env.PORT || 5000 // # Fall back to port 5000 if process.env.PORT is not set
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+//app.listen(5000,() => {
+  // console.log('Express server started at port 3000'); 
+//})
+
+
 app.use('/user', questionController);
+app.use('/',userController);
 app.use('/session', sessionController);
 app.use(express.static(__dirname+'/image'));
